13fa228502b59db57472fa3a55c906c051db2280
David Siddle <siddav83@gmail.com>
2025-03-21 20:57:22 +0000

    workflow(tests) undated tests, logic not correct

--- test.js
+++ test.js
@@ -13,10 +13,12 @@ if (add(-1, 1) !== 0) {
 
 if (subract(5, 3) !== 2) {
   console.error('Test failed: -5 - 3 should equal 2');
+  process.exit(1);
 }
 
-if (subract(3, -3) !== 0) {
-  console.error('Test failed 3 - -3 should equal 0');
+if (subract(3, -3) !== 6) {
+  console.error('Test failed 3 - -3 should equal 6');
+  process.exit(1);
 }
 
 console.log('All tests passed!');
